57f3a951c64f99102f1e89583693e6167585875e
DadmehrX <73789666+DadmehrX@users.noreply.github.com>
2021-01-08 19:25:02 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 ﻿const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json");
   
   const newUsers = [];
   
@@ -52,4 +51,4 @@ client.on('ready', () => {
 });
 
 
-client.login(config.token);
+client.login(process.env.token);
